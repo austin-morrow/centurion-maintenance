0ece0528a716f26556305b1be61b3a296af15b48
Austin Morrow <morrow.austin5@gmail.com>
2023-02-01 17:41:53 -0700

    Update WoodMilling.jsx

--- src/pages/WoodMilling.jsx
+++ src/pages/WoodMilling.jsx
@@ -2,7 +2,9 @@ export default function WoodMilling() {
   return (
     <>
       <section name='home' className='woodMillingImg'>
-        {/* Logo */}
+      <div>
+        <div className='text-white'>Wood Milling</div>
+      </div>
       </section>
     </>
   )
