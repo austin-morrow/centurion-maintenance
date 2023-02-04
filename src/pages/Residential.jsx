bc3c80e26a0f3d28e9a5abb525741e1691badfe9
Austin Morrow <morrow.austin5@gmail.com>
2023-02-03 17:49:21 -0700

    starting to build out residential page

--- src/pages/Residential.jsx
+++ src/pages/Residential.jsx
@@ -6,6 +6,34 @@ export default function Residential() {
         <div className='text-white'>Residential</div>
       </div>
       </section>
+      <div className='columns-3'>
+        <div>
+          Our services
+        </div>
+        <div>
+          <ul>
+            <li>Accessory Dwelling Units</li>
+            <li>Additions</li>
+            <li>Attic Restoration</li>
+            <li>Basement Remodeling</li>
+            <li>Bathroom Remodeling</li>
+            <li>Deck Building</li>
+            <li>Deck Repair</li>
+            <li>Dock Design & Construction</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Consultations</li>
+            <li>Drawing Services</li>
+            <li>Finishing</li>
+            <li>Finishing Selections</li>
+            <li>Permitting</li>
+            <li>Product Selection & Sourcing</li>
+          </ul>
+        </div>
+      </div>
+
     </>
   )
 }
