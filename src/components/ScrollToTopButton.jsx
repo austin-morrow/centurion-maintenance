05ce675161c94f35fd69ffc9c67d55aa263768dd
Austin Morrow <austinmorrowdev@gmail.com>
2024-02-18 10:40:59 -0700

    update footer and scroll to top button

--- src/components/ScrollToTopButton.jsx
+++ src/components/ScrollToTopButton.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function ScrollToTopButton() {
   const [isVisible, setIsVisible] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
 
   const handleScrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -9,9 +10,9 @@ function ScrollToTopButton() {
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollTop = window.pageYOffset;
-
-      setIsVisible(scrollTop > 0);
+      const currentScrollPos = window.pageYOffset;
+      setIsVisible(currentScrollPos < prevScrollPos && currentScrollPos > 0);
+      setPrevScrollPos(currentScrollPos);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -19,16 +20,16 @@ function ScrollToTopButton() {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [prevScrollPos]);
 
   return (
     <>
       {isVisible && (
         <button
-          className="fixed bottom-4 right-4 bg-[#a8a899] text-white p-2 rounded-full shadow-md"
+          className="fixed bottom-4 right-4 bg-[#a8a899] text-white p-1 rounded-md shadow-md"
           onClick={handleScrollToTop}
         >
-          <span class="material-symbols-outlined pt-1 px-1">arrow_upward</span>
+           <span class="material-symbols-outlined pt-1 px-1">arrow_upward</span>
         </button>
       )}
     </>
--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,18 +1,17 @@
-import React from "react"
-
+import React from "react";
+import Logo from "../assets/brand/cm-logo.png";
 
 const Footer = () => {
-
   return (
-    <div className='w-full bg-[#2C434D] text-[#A8A899] px-2'>
-      <div className='flex flex-col items-center max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-[text-[#A8A899]'>
-        <p className='py-4'>2023 Centurion Maintenance. All Rights Reserved</p>
-        <div>
-          Website by <a href="https://austinmorrow.me" target="_blank" rel="noreferrer">AM</a>
+    <div className="w-full  text-[#A8A899] px-2 py-2">
+      <div className="flex flex-col items-center max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-[text-[#A8A899] border-t">
+        <div className="flex-shrink-0">
+          <img className="h-14 pb-2 sm:pb-0" src={Logo} alt="Your Company" />
         </div>
+        <div>© 2024 Centurion Maintenance Inc. All rights reserved.</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
